2c9dd90d92695083b04412f7e7c0cac53b2e5eae
pauljoohyunkim <pauljoohyunkim@gmail.com>
2022-11-30 23:19:15 +0000

    Explicit Euler Scheme Complete

--- Lab/PDE/heatEquation.py
+++ Lab/PDE/heatEquation.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 # M Final time step
 # deltaT Time mesh size
 # deltaX Space mesh size
-def heatEvolve(a=-1,b=1,J=20,T=10,M=10000,u_a=0,u_b=0,u_initial = lambda x: 10):
+# Dirichlet Boundary Conditions u_a = u(a), u_b = u(b)
+def heatEvolveExplicitEuler(a=-1,b=1,J=20,T=10,M=10000,u_a=0,u_b=0,u_initial = lambda x: 10):
     deltaT = T/M        # Time mesh size
     deltaX = (b-a) / J  # Space mesh size
     cfl = deltaT / (deltaX ** 2)            # Want this below 1/2
@@ -25,6 +26,7 @@ def heatEvolve(a=-1,b=1,J=20,T=10,M=10000,u_a=0,u_b=0,u_initial = lambda x: 10):
         for j in range(1,J):
             ump1[0,j] = u[m,j] + cfl * (u[m,j+1] - 2 * u[m,j] + u[m,j-1])
         u = np.vstack([u, ump1])
+    return u
 
 
 if __name__ == "__main__":
@@ -33,32 +35,10 @@ if __name__ == "__main__":
     J = 20              # Final spacial point index
     T = 10              # Final time (in second)
     M = 10000           # Final time step
-    deltaT = T/M        # Time mesh size
+    u = heatEvolveExplicitEuler()
     deltaX = (b-a) / J  # Space mesh size
-    cfl = deltaT / (deltaX ** 2)            # Want this below 1/2
-    if(cfl > 0.5):
-        print(f"Warning: the CFL number is {cfl}, so stability is not guaranteed")
-    # Dirichlet Boundary Conditions u(a) = 0, u(b) = 0
-    u_a = 0
-    u_b = 0
-
-
-    # The indexed variables
-    # x_j = a + j * deltaX
-    # t_m = m * deltaT
-
-
-    # Initial Data u_0(x) = -10*(x-a)(x-b)
-    u_initial = lambda x: -10 *(x-a)*(x-b)
-    u = np.array([[u_a] + [u_initial(a + j * deltaX) for j in range(1,J)] + [u_b]], dtype=float)
-    for m in range(M):
-        ump1 = np.array([[u_a] + [0 for _ in range(1, J)] + [u_b]], dtype=float)
-        # Explicit Scheme
-        for j in range(1,J):
-            ump1[0,j] = u[m,j] + cfl * (u[m,j+1] - 2 * u[m,j] + u[m,j-1])
-        u = np.vstack([u, ump1])
     
-    for m in range(0, M, 100):
+    for m in range(0, M, 50):
         plt.plot([a + j * deltaX for j in range(J+1)], u[m])
     plt.show()
 
