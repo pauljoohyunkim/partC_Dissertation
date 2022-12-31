fbe52f2bc0b1e87530090b0d6f6ebc8f0c4bd12a
pauljoohyunkim <pauljoohyunkim@gmail.com>
2022-12-31 11:55:03 +0100

    Nonlinear Periodic with Test

--- Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
+++ Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
@@ -8,8 +8,44 @@ import matplotlib.pyplot as plt
 # deltaT Time mesh size
 # deltaX Space mesh size
 def nonlinearEvolve(a=-1,b=1,J=20,T=10000,M=10000000,u_initial = lambda x: 1 if (x < 1 and -1 < x) else 0):
-    pass
+    deltaT = T/M        # Time mesh size
+    deltaX = (b-a) / J  # Space mesh size
+    cfl = deltaT / (deltaX ** 2)            # Want this below 1/2
+    if(cfl > 0.5):
+        print(f"Warning: the CFL number is {cfl}, so stability is not guaranteed")
+    
+    # Explicit Scheme
+    u = [[u_initial(a + j * deltaX) for j in range(0,J + 1)]]
+    for m in range(M):
+        ump1 = [[0 for _ in range(0, J + 1)]]
+        # Explicit Scheme
+        for j in range(1,J):
+            #ump1[0][j] = u[m][j] + cfl * (u[m][j+1] - 2 * u[m][j] + u[m][j-1])
+            ump1[0][j] = cfl * (2 * u[m][j] * (u[m][j+1] - 2 * u[m][j] + u[m][j-1]) + 0.5 * ((u[m][j+1] - u[m][j-1])**2))
+        ump1[0][0] = cfl * (2 * u[m][0] * (u[m][1] - 2 * u[m][0] + u[m][J]) + 0.5 * ((u[m][1] - u[m][J])**2))
+        #ump1[0][0] = u[m][0] + cfl * (u[m][1] - 2 * u[m][0] + u[m][J])
+        ump1[0][J] = cfl * (2 * u[m][J] * (u[m][0] - 2 * u[m][J] + u[m][J-1]) + 0.5 * ((u[m][0] - u[m][J-1])**2))
+        #ump1[0][J] = u[m][J] + cfl * (u[m][0] - 2 * u[m][J] + u[m][J-1])
+        u += ump1
+    return u
 
 
 if __name__ == "__main__":
-    pass
+    a = -1
+    b = 1
+    J = 80                 # Final spacial point index
+    T = 100                # Final time (in second)
+    M = 400000             # Final time step
+    deltaT = T/M        # Time mesh size
+    deltaX = (b-a) / J  # Space mesh size
+    u_initial = lambda x : -(x-1)*(x+1)*10
+    u = nonlinearEvolve(a, b, J, T, M, u_initial=u_initial)
+    deltaX = (b-a) / J  # Space mesh size
+    
+    plt.rcParams['text.usetex'] = True
+
+    for m in range(0, M, 10):
+        plt.plot([a + j * deltaX for j in range(J+1)], u[m])
+    plt.suptitle(r"$u_{t} = \Delta (u^2)$ in $(x,t) \in \left[" + f"({a},{b}), (0,{T})" + r"\right]$" + "\n" + f"{J + 1} spacial mesh points, {M+1} time mesh points, {J * (M+1)} points overall")
+    plt.show()
+
