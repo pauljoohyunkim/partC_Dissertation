98de5c7d87279586334187df4b123a30f99bebea
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-01-07 00:10:58 +0100

    Scheme without simplification as an option in the experiment

--- Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
+++ Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
@@ -26,6 +26,27 @@ def nonlinearEvolve1(a=-1,b=1,J=20,T=10000,M=10000000,u_initial = lambda x: 1 if
         u += ump1
     return u
 
+def nonlinearEvolve2(a=-1,b=1,J=20,T=10000,M=10000000,u_initial = lambda x: 1 if (x < 1 and -1 < x) else 0):
+    deltaT = T/M        # Time mesh size
+    deltaX = (b-a) / J  # Space mesh size
+    cfl = deltaT / (deltaX ** 2)            # Want this below 1/2
+    if(cfl > 0.5):
+        print(f"Warning: the CFL number is {cfl}, so stability is not guaranteed")
+    
+    # Explicit Scheme
+    u = [[u_initial(a + j * deltaX) for j in range(0,J + 1)]]
+    for m in range(M):
+        ump1 = [[0 for _ in range(0, J + 1)]]
+        # Explicit Scheme
+        for j in range(1,J):
+            #ump1[0][j] = u[m][j] + cfl * (2 * u[m][j] * (u[m][j+1] - 2 * u[m][j] + u[m][j-1]) + 0.5 * ((u[m][j+1] - u[m][j-1])**2))
+            ump1[0][j] = u[m][j] + cfl * (u[m][j+1] ** 2 - 2 * u[m][j] ** 2 + u[m][j-1] ** 2)
+        ump1[0][0] = u[m][0] + cfl * (u[m][1] ** 2 - 2 * u[m][0] ** 2 + u[m][J] ** 2)
+        ump1[0][J] = u[m][J] + cfl * (u[m][0] ** 2 - 2 * u[m][J] ** 2 + u[m][J-1] ** 2)
+        #ump1[0][0] = u[m][j] + cfl * (2 * u[m][0] * (u[m][1] - 2 * u[m][0] + u[m][J]) + 0.5 * ((u[m][1] - u[m][J])**2))
+        #ump1[0][J] = u[m][j] + cfl * (2 * u[m][J] * (u[m][0] - 2 * u[m][J] + u[m][J-1]) + 0.5 * ((u[m][0] - u[m][J-1])**2))
+        u += ump1
+    return u
 
 if __name__ == "__main__":
     a = -1
@@ -36,7 +57,7 @@ if __name__ == "__main__":
     deltaT = T/M        # Time mesh size
     deltaX = (b-a) / J  # Space mesh size
     u_initial = lambda x: 1 if (x < 1 and -1 < x) else 0
-    u = nonlinearEvolve1(a, b, J, T, M, u_initial)
+    u = nonlinearEvolve2(a, b, J, T, M, u_initial)
     deltaX = (b-a) / J  # Space mesh size
     
     plt.rcParams['text.usetex'] = True
