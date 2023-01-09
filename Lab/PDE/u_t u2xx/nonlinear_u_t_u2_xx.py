1fb0304ba69d3ddfa4d846ad446a012d76f9721a
Paul Kim <pauljoohyunkim@gmail.com>
2023-01-09 12:00:17 +0100

    Before reaching equilibrium, the nonlinear equation forms a parabola-like shape

--- Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
+++ Lab/PDE/u_t u2xx/nonlinear_u_t_u2_xx.py
@@ -51,8 +51,8 @@ def nonlinearEvolve2(a=-1,b=1,J=20,T=10000,M=10000000,u_initial = lambda x: 1 if
 if __name__ == "__main__":
     a = -1
     b = 1
-    J = 40                 # Final spacial point index
-    T = 10                # Final time (in second)
+    J = 400                 # Final spacial point index
+    T = 0.15                # Final time (in second)
     M = 80000             # Final time step
     deltaT = T/M        # Time mesh size
     deltaX = (b-a) / J  # Space mesh size
