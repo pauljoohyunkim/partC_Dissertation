f1d2db2b11fc4444a8a9c3f03a2ce48b43485844
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-01-28 11:47:56 +0000

    GradientFlow  and differential separate

--- Lab/PDE/cuda/src/Curve-Repulsion/curve-repulsion-1.cu
+++ Lab/PDE/cuda/src/Curve-Repulsion/curve-repulsion-1.cu
@@ -2,6 +2,11 @@
 #include "../solver.hpp"
 #include "../geometric-objects.hpp"
 
+#define DELTA_X 0.1
+#define DELTA_T 0.05
+#define LAMBDA 0.1
+
+__global__ static void repulsiveCurveDifferential(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J);
 __global__ static void repulsiveCurveGradientFlow(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J);
 
 int main()
@@ -18,10 +23,36 @@ int main()
     dim3 grid(C.J, 3);
 
     /* Gradient Flow */
+    repulsiveCurveDifferential<<<grid, 1>>>(C.dev_x, C.dev_y, C.dev_z, C.dev_energyMatrixFlattened, C.J);
     repulsiveCurveGradientFlow<<<grid, 1>>>(C.dev_x, C.dev_y, C.dev_z, C.dev_energyMatrixFlattened, C.J);
     C.flushFromDevice();
 
     return 0;
+}
+
+__global__ static void repulsiveCurveDifferential(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J)
+{
+    /* Point Index */
+    int i = blockIdx.x;
+    /* Coordinate Index j = 0, 1, 2 for x, y, z respectively */
+    int j = blockIdx.y;
+    //const double deltaX = 0.1;
+    if (i < J)
+    {
+        unsigned int flattenedPos = J * j + i;
+        if (j == 0)
+        {
+            cuDifferential(dev_x, dev_y, dev_z, i, DELTA_X, 0.0, 0.0, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+        if (j == 1)
+        {
+            cuDifferential(dev_x, dev_y, dev_z, i, 0.0, DELTA_X, 0.0, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+        if (j == 2)
+        {
+            cuDifferential(dev_x, dev_y, dev_z, i, 0.0, 0.0, DELTA_X, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+    }
 }
 
 __global__ static void repulsiveCurveGradientFlow(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J)
@@ -30,22 +61,21 @@ __global__ static void repulsiveCurveGradientFlow(double* dev_x, double* dev_y, 
     int i = blockIdx.x;
     /* Coordinate Index j = 0, 1, 2 for x, y, z respectively */
     int j = blockIdx.y;
-    //printf("repulsiveCurveGradientFlow(.): (i,j)=(%d,%d)\n", i, j);
-    const double deltaX = 0.1;
+    //const double deltaX = 0.1;
     if (i < J)
     {
         unsigned int flattenedPos = J * j + i;
         if (j == 0)
         {
-            dev_energyMatrixFlattened[flattenedPos] = cuDifferential(dev_x, dev_y, dev_z, i, deltaX, 0.0, 0.0, J);
+            dev_x[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_x[i];
         }
         if (j == 1)
         {
-            dev_energyMatrixFlattened[flattenedPos] = cuDifferential(dev_x, dev_y, dev_z, i, 0.0, deltaX, 0.0, J);
+            dev_y[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_y[i];
         }
         if (j == 2)
         {
-            dev_energyMatrixFlattened[flattenedPos] = cuDifferential(dev_x, dev_y, dev_z, i, 0.0, 0.0, deltaX, J);
+            dev_z[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_z[i];
         }
     }
 }
