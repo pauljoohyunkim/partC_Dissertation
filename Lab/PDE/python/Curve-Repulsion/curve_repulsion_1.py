a6b0a199bf5c7c10fab1f7738e67314993767e4a
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-01-28 10:08:06 +0000

    All differentials seems to match up with python

--- Lab/PDE/python/Curve-Repulsion/curve_repulsion_1.py
+++ Lab/PDE/python/Curve-Repulsion/curve_repulsion_1.py
@@ -18,18 +18,19 @@ if __name__ == "__main__":
     x4 = np.array([5.5, -6.5, 0.0])
     '''
 
-    x1 = np.array([1,0,-1])
-    x2 = np.array([2,2,-2])
-    x3 = np.array([3,4,3])
-    x4 = np.array([4,6,-4])
-    x5 = np.array([5,8,5])
-    x6 = np.array([6,-1,0.6])
+    x1 = np.array([1,0,-1], dtype=float)
+    x2 = np.array([2,2,-2], dtype=float)
+    x3 = np.array([3,4,3], dtype=float)
+    x4 = np.array([4,6,-4], dtype=float)
+    x5 = np.array([5,8,5], dtype=float)
+    x6 = np.array([6,-1,0.6], dtype=float)
 
     c = [x1, x2, x3, x4, x5, x6]
 
     discretekernelenergyOrigin = discrete_kernel.energy(c)
 
-    d = [np.array([1.1, 0, -1]), x2, x3, x4, x5, x6]
+    c[5] += np.array([0.0,0,0.1])
+    d = [x1, x2, x3, x4, x5, x6]
 
     discretekernelenergyPerturbed = discrete_kernel.energy(d)
 
