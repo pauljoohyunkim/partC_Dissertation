4ebfdd55c100906cdb0787b37064863c435fbb3e
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-02-04 19:59:18 +0000

    Circle Simple with central energy

---
+++ Lab/PDE/cuda/src/Curve-Repulsion/curve-repulsion-simple-2-circle.cu
@@ -0,0 +1,131 @@
+#include "../solver.hpp"
+#include "../geometric-objects.hpp"
+#include "../export.hpp"
+#include <cmath>
+#include <fstream>
+
+#define DELTA_X 0.1
+#define DELTA_T 0.005
+#define LAMBDA 0.1
+#define M 10000
+
+#define PLOT_FREQUENCY 10
+#define AZIMUTHAL_SPEED 0.5
+#define ELEVATION 3
+
+__global__ static void repulsiveCurveDifferential(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J);
+__global__ static void repulsiveCurveGradientFlow(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J);
+
+int main()
+{
+    /* Generate curve */
+
+    std::vector<double> x {};
+    std::vector<double> y {};
+    std::vector<double> z {};
+
+    /* Example 3: Just a circle */
+    const int resolution { 40 };
+    for (auto i = 0; i < resolution; i++)
+    {
+        double theta = 2 * M_PI * (double) i / resolution;
+        x.push_back(cos(theta));
+        y.push_back(sin(theta));
+        z.push_back(0);
+    }
+
+    cuRepulsiveCurve C(x, y, z);
+    C.cudafy();
+
+
+    /* Parallel Pool */
+    dim3 grid(C.J, 3);
+
+    /* Export to json */
+    std::ofstream jsonX("x.json");
+    std::ofstream jsonY("y.json");
+    std::ofstream jsonZ("z.json");
+    jsonX << "[";
+    jsonY << "[";
+    jsonZ << "[";
+
+    /* Gradient Flow */
+    for (auto t = 0; t < M; t++)
+    {
+        repulsiveCurveDifferential<<<grid, 1>>>(C.dev_x, C.dev_y, C.dev_z, C.dev_energyMatrixFlattened, C.J);
+        repulsiveCurveGradientFlow<<<grid, 1>>>(C.dev_x, C.dev_y, C.dev_z, C.dev_energyMatrixFlattened, C.J);
+        C.flushFromDevice();
+        if (t % PLOT_FREQUENCY == 0)
+        {
+            std::cout << "Progress: " << t << "/" << M << "(" << (float) t / M * 100 << "%)" << std::endl;
+            /* Export to json */
+            if (t != 0)
+            {
+                jsonX << ",\n";
+                jsonY << ",\n";
+                jsonZ << ",\n";
+            }
+            vectorParse(jsonX, C.x, C.J);
+            vectorParse(jsonY, C.y, C.J);
+            vectorParse(jsonZ, C.z, C.J);
+        }
+    }
+    jsonX << "]";
+    jsonY << "]";
+    jsonZ << "]";
+    jsonX.close();
+    jsonY.close();
+    jsonZ.close();
+
+    return 0;
+}
+
+__global__ static void repulsiveCurveDifferential(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J)
+{
+    /* Point Index */
+    int i = blockIdx.x;
+    /* Coordinate Index j = 0, 1, 2 for x, y, z respectively */
+    int j = blockIdx.y;
+    //const double deltaX = 0.1;
+    if (i < J)
+    {
+        unsigned int flattenedPos = J * j + i;
+        if (j == 0)
+        {
+            cuDifferentialSimpleCentral(dev_x, dev_y, dev_z, i, DELTA_X, 0.0, 0.0, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+        if (j == 1)
+        {
+            cuDifferentialSimpleCentral(dev_x, dev_y, dev_z, i, 0.0, DELTA_X, 0.0, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+        if (j == 2)
+        {
+            cuDifferentialSimpleCentral(dev_x, dev_y, dev_z, i, 0.0, 0.0, DELTA_X, J, &dev_energyMatrixFlattened[flattenedPos]);
+        }
+    }
+}
+
+__global__ static void repulsiveCurveGradientFlow(double* dev_x, double* dev_y, double* dev_z, double* dev_energyMatrixFlattened, double J)
+{
+    /* Point Index */
+    int i = blockIdx.x;
+    /* Coordinate Index j = 0, 1, 2 for x, y, z respectively */
+    int j = blockIdx.y;
+    //const double deltaX = 0.1;
+    if (i < J)
+    {
+        unsigned int flattenedPos = J * j + i;
+        if (j == 0)
+        {
+            dev_x[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_x[i];
+        }
+        if (j == 1)
+        {
+            dev_y[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_y[i];
+        }
+        if (j == 2)
+        {
+            dev_z[i] += -dev_energyMatrixFlattened[flattenedPos] / DELTA_X * DELTA_T - LAMBDA * DELTA_T * dev_z[i];
+        }
+    }
+}
--- Lab/PDE/cuda/src/solver.cu
+++ Lab/PDE/cuda/src/solver.cu
@@ -653,6 +653,172 @@ __device__ double cuDifferentialSimple(double* dev_x, double* dev_y, double* dev
     *pVar = de / 2;
 }
 
+
+
+__device__ void cuDifferentialSimpleCentral(double* dev_x, double* dev_y, double* dev_z, int index, double diffx, double diffy, double diffz, unsigned int J, double* pVar)
+{
+    /* 2-Pass:
+     On the first pass, it perturbs the curve a bit temporarily and computes the energy.
+     On the second pass, it perturbs the curve in the opposite direction, then computes the energy, subtracting off kernel points
+     then it divides by 2.*/
+
+    *pVar = 0;
+
+    index = ((index % (int) J) + J) % J;
+
+    /* Energy of perturbed curve */
+    for (int i = 0; i < J; i++)
+    {
+        for (int j = 0; j < J; j++)
+        {
+            if (abs(i - j) > 1 && abs(i - j + (int) J) > 1 && abs(i - j - (int) J) > 1)
+            {
+                int ip1 = (i + 1) % J;
+                int jp1 = (j + 1) % J;
+                /* x_i, x_j */
+                double xix = dev_x[i];
+                double xiy = dev_y[i];
+                double xiz = dev_z[i];
+                double xjx = dev_x[j];
+                double xjy = dev_y[j];
+                double xjz = dev_z[j];
+                /* Perturbation */
+                if (i == index)
+                {
+                    xix += diffx;
+                    xiy += diffy;
+                    xiz += diffz;
+                }
+                if (j == index)
+                {
+                    xjx += diffx;
+                    xjy += diffy;
+                    xjz += diffz;
+                }
+
+                /* x_{i+1}, x_{j+1} */
+                double xipx = dev_x[ip1];
+                double xipy = dev_y[ip1];
+                double xipz = dev_z[ip1];
+                double xjpx = dev_x[jp1];
+                double xjpy = dev_y[jp1];
+                double xjpz = dev_z[jp1];
+                
+                /* Perturbation */
+                if (ip1 == index)
+                {
+                    xipx += diffx;
+                    xipy += diffy;
+                    xipz += diffz;
+                }
+                if (jp1 == index)
+                {
+                    xjpx += diffx;
+                    xjpy += diffy;
+                    xjpz += diffz;
+                }
+
+                /* xI, xJ */
+                double xIx = xipx - xix;
+                double xIy = xipy - xiy;
+                double xIz = xipz - xiz;
+                double xJx = xjpx - xjx;
+                double xJy = xjpy - xjy;
+                double xJz = xjpz - xjz;
+
+                /* lI, lJ */
+                double lI = l2norm3D(xIx, xIy, xIz);
+                double lJ = l2norm3D(xJx, xJy, xJz);
+
+                *pVar += kernelFunctionSimpleCentral(xix, xiy, xiz, xipx, xipy, xipz,
+                        xjx, xjy, xjz, xjpx, xjpy, xjpz) * lI * lJ;
+            }
+        }
+    }
+
+    /* Energy of curve perturbed in the opposite direction subtracted off */
+    for (int i = 0; i < J; i++)
+    {
+        for (int j = 0; j < J; j++)
+        {
+            if (abs(i - j) > 1 && abs(i - j + (int) J) > 1 && abs(i - j - (int) J) > 1)
+            {
+                int ip1 = (i + 1) % J;
+                int jp1 = (j + 1) % J;
+                /* x_i, x_j */
+                double xix = dev_x[i];
+                double xiy = dev_y[i];
+                double xiz = dev_z[i];
+                double xjx = dev_x[j];
+                double xjy = dev_y[j];
+                double xjz = dev_z[j];
+                /* Perturbation */
+                if (i == index)
+                {
+                    xix -= diffx;
+                    xiy -= diffy;
+                    xiz -= diffz;
+                }
+                if (j == index)
+                {
+                    xjx -= diffx;
+                    xjy -= diffy;
+                    xjz -= diffz;
+                }
+
+                /* x_{i+1}, x_{j+1} */
+                double xipx = dev_x[ip1];
+                double xipy = dev_y[ip1];
+                double xipz = dev_z[ip1];
+                double xjpx = dev_x[jp1];
+                double xjpy = dev_y[jp1];
+                double xjpz = dev_z[jp1];
+                /* Perturbation */
+                if (ip1 == index)
+                {
+                    xipx -= diffx;
+                    xipy -= diffy;
+                    xipz -= diffz;
+                }
+                if (jp1 == index)
+                {
+                    xjpx -= diffx;
+                    xjpy -= diffy;
+                    xjpz -= diffz;
+                }
+
+                /* xI, xJ */
+                double xIx = xipx - xix;
+                double xIy = xipy - xiy;
+                double xIz = xipz - xiz;
+                double xJx = xjpx - xjx;
+                double xJy = xjpy - xjy;
+                double xJz = xjpz - xjz;
+
+                /* lI, lJ */
+                double lI = l2norm3D(xIx, xIy, xIz);
+                double lJ = l2norm3D(xJx, xJy, xJz);
+
+                *pVar -= kernelFunctionSimpleCentral(xix, xiy, xiz, xipx, xipy, xipz,
+                        xjx, xjy, xjz, xjpx, xjpy, xjpz) * lI * lJ;
+            }
+        }
+    }
+    *pVar = (*pVar) / 2;
+}
+
+
+__device__ double kernelFunctionSimpleCentral(double xix, double xiy, double xiz, double xipx, double xipy, double xipz, double xjx, double xjy, double xjz, double xjpx, double xjpy, double xjpz, double alpha)
+{
+    double kernelVal { 0 };
+    kernelVal += 1 / pow(l2norm3D(xix - xjx, xiy - xjy, xiz - xjz), alpha);
+    kernelVal += 1 / pow(l2norm3D(xix - xjpx, xiy - xjpy, xiz - xjpz), alpha);
+    kernelVal += 1 / pow(l2norm3D(xipx - xjx, xipy - xjy, xipz - xjz), alpha);
+    kernelVal += 1 / pow(l2norm3D(xipx - xjpx, xipy - xjpy, xipz - xjpz), alpha);
+
+    return kernelVal / 4;
+}
+
 __device__ void cross(double x1, double y1, double z1, double x2, double y2, double z2, double &x3, double &y3, double &z3)
 {
     x3 = y1 * z2 - y2 * z1;
